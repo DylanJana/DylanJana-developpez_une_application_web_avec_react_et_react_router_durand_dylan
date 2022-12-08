7d5de0d0606dbf73c54ad57f4092a43a72bea37f
Dylandurand <77446537+Dylandurand@users.noreply.github.com>
2022-12-08 19:02:36 +0100

    mise en place de la page detail

--- developpez_une_application_web_react_et_react_router_durand_dylan/src/components/Detail/Detail.js
+++ developpez_une_application_web_react_et_react_router_durand_dylan/src/components/Detail/Detail.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Slider from "../Slider/Slider";
+import ListCards from "../../data.json";
+import TitleAddress from "../TitleAddress/TitleAddress";
+import HostInfo from "../HostInfo/HostInfo";
+import DetailAccordions from "../DetailAccordions/DetailAccordions";
+import Page404 from "../Page404/Page404";
 
-export default function Detail() {
+
+function Detail() {
+  const parameters = useParams();
+  const [card, setCard] = useState(null);
+
+  useEffect(() => {
+    ListCards.map((lodging) => {
+      if (lodging.id === parameters.lodgingid) {
+        setCard(lodging);
+      }
+    });
+  });
   return (
     <div>
-      Detail
+      {card !== null ? (
+        <>
+          <Header />
+          <div>
+            <Slider cardinfo={card} />
+            <div className="container my-4">
+              <div className="row">
+                <TitleAddress
+                  title={card.title}
+                  localisation={card.location}
+                  tags={card.tags}
+                />
+                <HostInfo hostinfo={card.host} rating={card.rating} />
+                <DetailAccordions
+                  equipmentProps={card.equipments}
+                  descriptionProps={card.description}
+                />
+              </div>
+            </div>
+          </div>
+          <Footer />
+        </>
+      ) : (
+        <Page404 />
+      )}
     </div>
-  )
+  );
 }
+
+export default Detail;
