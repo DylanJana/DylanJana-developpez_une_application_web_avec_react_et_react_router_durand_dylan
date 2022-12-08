56615b55c8e1e48ff8e6164f7a7309cdeb95bb86
Dylandurand <77446537+Dylandurand@users.noreply.github.com>
2022-12-08 17:09:50 +0100

    Fin de la home

--- developpez_une_application_web_react_et_react_router_durand_dylan/src/components/LodgingCard/LodgingCard.js
+++ developpez_une_application_web_react_et_react_router_durand_dylan/src/components/LodgingCard/LodgingCard.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import {NavLink} from 'react-router-dom';
+import './lodgingcard.css';
 
 class LodgingCard extends Component {
   render() {
     return (
-      <div>
-        LodgingCard
+      <div className='col-12 col-md-6 col-lg-4 my-2 mb-5 card'>
+        <NavLink to={'/lodging/'+this.props.id}>
+        <div className='card-body' style={{ backgroundImage: "url(" + this.props.coverpicture + ")", backgroundSize:'cover', backgroundPosition:'center' }}>
+            <span className='card-text'>{this.props.title}</span>
+        </div>
+        </NavLink>
+        
       </div>
     )
   }
 }
 
+
 export default LodgingCard;
+
