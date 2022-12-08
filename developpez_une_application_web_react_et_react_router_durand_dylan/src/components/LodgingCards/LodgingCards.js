b66bc678cca9d7ec74f2a7cc8d33aea3626fc0bb
Dylandurand <77446537+Dylandurand@users.noreply.github.com>
2022-12-08 16:38:19 +0100

    Mise en place des props Lodgingcard

--- developpez_une_application_web_react_et_react_router_durand_dylan/src/components/LodgingCards/LodgingCards.js
+++ developpez_une_application_web_react_et_react_router_durand_dylan/src/components/LodgingCards/LodgingCards.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import LodgingCard from '../LodgingCard/LodgingCard.js';
+import './lodgingcards.css';
+import LodgingDatas from '../../data.json';
 
 class LodgingCards extends Component {
   render() {
     return (
       <div>
-        LodgingCards
+        <div className='container p-5 my-3 cards'>
+          <div className='row gx-5'>
+            {
+              LodgingDatas.map((lodging) => {
+                return (
+                  <LodgingCard key={lodging.id} id={lodging.id} title={lodging.title} coverpicture = {lodging.cover} />
+                )
+              })
+            }
+          </div>
+        </div>
       </div>
     )
   }
