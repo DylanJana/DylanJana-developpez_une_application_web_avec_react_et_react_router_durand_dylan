7bc24c8b7634081c6706ed70d3852696f5090534
Dylan Durand <dylan.durand88@outlook.fr>
2022-12-24 16:01:56 +0100

    Ajout d'explication sur la page Detail.js

--- developpez_une_application_web_react_et_react_router_durand_dylan/src/components/Detail/Detail.js
+++ developpez_une_application_web_react_et_react_router_durand_dylan/src/components/Detail/Detail.js
@@ -12,9 +12,9 @@ import DetailAccordions from "../DetailAccordions/DetailAccordions";
 import Page404 from "../Page404/Page404";
 // Utilisation d'un composant fonction pour pouvoir utiliser les fonctions useParams (), useEffet(), useState() . Points communs à ttes ces fct, c'est que leurs valeurs sont modifiables.
 export default function Detail() {
-//  useParams() crochet(=hook) permet de récupérer les paramètres dynamiques (ici l'id du logement ) de l'url actuelle qui ont été mis en correspondance par le <Route path>
+//  useParams() crochet(=hook) permet de récupérer les paramètres dynamiques (ici l'id du logement )
   const parameters = useParams();
-  // ajout d'un state pour pouvoir mettre la condition d'affichage de la page 404
+  // ajout d'un state + la fonction de modification du state
   const [card, setCard] = useState(null);
   useEffect(() => {
     ListCards.map((lodging) => {
@@ -27,11 +27,11 @@ export default function Detail() {
     <div>
       {card !== null ? (
         // Lorsque nous voulons utiliser plus d'un composant après return (plus haut), nous devons "l'emballer avec 2 chevrons au début et 2 à la fin : <>, </>".
-        //<Slider cardinfo={card} /> = nous créons un props pour envoyer les informations de la carte au composant Slider. Nous pouvons lui donner le nom que nous voulons. Ici, nous définissons son nom comme cardinfo. Nous avons associons la valeur de cardinfo à la variable card. Ainsi, nous avons envoyé les informations de l'hébergement cliqué au composant slider.
+        //<Slider cardinfo={card} /> = nous créons un props pour envoyer les informations de la carte au composant Slider. Nous pouvons lui donner le nom que nous voulons
         <>
           <Header />
           <div>
-            {/* création et envoi des props(ex: cardinfo, title,...) et je les envoie aux différents componsants pour avoir accès à tous les datas  */}
+            {/* création et envoi des props(ex: cardinfo, title,...), cette action me permet de transmettre des données que je pourrais réutiliser dans le composant enfant */}
             <Slider cardinfo={card} />
             <div className="container my-4">
               <div className="row">
@@ -52,6 +52,7 @@ export default function Detail() {
           <Footer />
         </>
       ) : (
+        /** Si aucune card ne correspond à l'ID en cours, je renvoie l'utilisateur sur la 404 */
         <Page404 />
       )}
     </div>
