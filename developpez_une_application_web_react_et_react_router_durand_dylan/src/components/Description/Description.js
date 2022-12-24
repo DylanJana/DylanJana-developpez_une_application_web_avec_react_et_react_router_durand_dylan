fd1d70bc530156249461fa2fc4e2c5bd80eb9560
Dylan Durand <dylan.durand88@outlook.fr>
2022-12-24 18:30:34 +0100

    Mise en place du composant Description.js et de son css

--- developpez_une_application_web_react_et_react_router_durand_dylan/src/components/Description/Description.js
+++ developpez_une_application_web_react_et_react_router_durand_dylan/src/components/Description/Description.js
@@ -1,12 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import chevronhaut from "../../images/arrow-up.png";
+import chevronbas from "../../images/arrow-down.png";
+import "./description.css";
 
 class Description extends Component {
+  // avec React la création d'un constructor est obligatoire pour créer un state ou l'utiliser.
+  constructor(props) {
+    //récupère les particularités de la class parent
+    super(props);
+    // State signifie statut. Avec React par ex obtenir un statut cliqué ou non cliqué pour un bouton on crée un state.
+    this.state = {
+      //création d'un state "descriptionOn" pour gérer le controle lors d'un clic.
+      descriptionOn: false
+    };
+  }
+  changeDescriptionState = () => {
+    this.setState({
+      // change le statut de descriptionOne par son inverse, s'il est true, il deviendra false, ainsi de suite.
+      descriptionOn: !this.state.descriptionOn,
+    });
+  };
   render() {
+    const { description } = this.props;
     return (
-      <div>
-        Description
+      <div className="col-12 col-lg-6 description-area">
+        <div
+          className="description-title"
+          onClick={this.changeDescriptionState}
+        >
+          <div className="description-text">Description</div>
+          <div>
+            {this.state.descriptionOn ? (
+              <img src={chevronbas} className="img-fluid arrow-img" />
+            ) : (
+              <img src={chevronhaut} className="img-fluid arrow-img" />
+            )}
+          </div>
+        </div>
+        {this.state.descriptionOn ? (
+          <div className="description-bigtext">{description}</div>
+        ) : null}
       </div>
-    )
+    );
   }
 }
 
--- developpez_une_application_web_react_et_react_router_durand_dylan/src/components/DetailAccordions/DetailAccordions.js
+++ developpez_une_application_web_react_et_react_router_durand_dylan/src/components/DetailAccordions/DetailAccordions.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Description from "../Description/Description";
+import Equipments from "../Equipments/Equipments";
 
 class DetailAccordions extends Component {
   render() {
+    const { equipmentProps, descriptionProps } = this.props;
     return (
-      <div>
-        Details accordeon
+      <div className="container my-4">
+        <div className="row">
+          <Description description={descriptionProps} />
+          <Equipments equipments={equipmentProps} />
+        </div>
       </div>
-    )
+    );
   }
 }
 
